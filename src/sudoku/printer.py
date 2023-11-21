a0015338c82303232a8989e17b9158211e5c0083
Ilja Heitlager <ilja@heitlager.com>
2023-11-21 21:02:29 +0100

    adding second csp solver

--- src/sudoku/printer.py
+++ src/sudoku/printer.py
@@ -10,3 +10,14 @@ def display_grid(grid):
             print_line += str(grid[i][j])
             print_line += " | " if j in [2, 5] else " "
         print(print_line)
+
+
+def display_pylist(grid):
+    '''
+    Display the grid as a python readable list
+    '''
+    print("sudoku_grid = [")
+    for row in grid[:-1]:
+        print("    [%s]," % ', '.join([str(x) for x in row]))
+    print("    [%s]" % ', '.join([str(x) for x in grid[-1]]))
+    print("]")
--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,7 +1,7 @@
 from contextlib import redirect_stdout
 import io
 
-from sudoku.printer import display_grid
+from sudoku.printer import display_grid, display_pylist
 
 sudoku_grid = [
     [0, 6, 0, 0, 0, 0, 1, 9, 0],
@@ -28,9 +28,30 @@ sudoku_string = '''0 6 0 | 0 0 0 | 1 9 0
 0 0 0 | 0 0 0 | 4 0 0 
 '''
 
+python_string = '''sudoku_grid = [
+    [0, 6, 0, 0, 0, 0, 1, 9, 0],
+    [0, 0, 2, 6, 1, 0, 0, 0, 4],
+    [7, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 7, 0, 0, 1, 0],
+    [0, 0, 6, 0, 8, 3, 0, 0, 0],
+    [5, 4, 0, 0, 6, 0, 0, 0, 3],
+    [0, 8, 0, 0, 2, 7, 0, 3, 9],
+    [0, 0, 0, 4, 0, 0, 0, 7, 8],
+    [0, 0, 0, 0, 0, 0, 4, 0, 0]
+]
+'''
+
 def test_display_grid():
     f = io.StringIO()
     with redirect_stdout(f):
         display_grid(sudoku_grid)
         out = f.getvalue()
         assert out == sudoku_string
+
+
+def test_display_pygrid():
+    f = io.StringIO()
+    with redirect_stdout(f):
+        display_pylist(sudoku_grid)
+        out = f.getvalue()
+        assert out == python_string
